0ed52c1ce42dfa46eae4b005c5927d29c5507c3f
Abel <lewheabel16@gmail.com>
2025-11-29 17:48:13 +0100

    retouches

--- front/src/Index.jsx
+++ front/src/Index.jsx
@@ -438,6 +438,116 @@ const TestimonialSection = () => {
     );
 }
 
+const KumiEagles = ()=>{
+    return(
+        <section className="grid grid-cols-1 lg:grid-cols-2">
+            {/* Section KUMI - Gauche */}
+                <div className="relative min-h-[600px] lg:min-h-[700px] bg-gray-300 flex flex-col items-center justify-between py-12 px-8">
+                    {/* Titre */}
+                    <div className="text-center mb-8">
+                        <h2 className="text-4xl md:text-3xl lg:text-5xl font-bold text-white leading-tight drop-shadow-lg">
+                            TU ES UNE SŒUR ?<br />
+                            REJOINS LES KUMI !
+                        </h2>
+                    </div>
+
+                    {/* Image */}
+                    <div className="flex-1 w-full max-w-2xl mb-8">
+                        <img
+                            src={unesœur}
+                            alt="Les Kumi"
+                            className="w-full h-full object-cover rounded-lg shadow-2xl"
+                        />
+                    </div>
+
+                    {/* Bouton */}
+                    <button className="px-8 py-4 border-2 border-white text-white rounded-md hover:bg-white hover:text-[#1e3a8a] transition-all duration-300 font-medium text-lg flex items-center gap-2">
+                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
+                        </svg>
+                        Rejoindre ici
+                    </button><br />
+                    <h2 className="text-2xl md:text-2xl lg:text-2xl font-bold text-gray-600 leading-tight drop-shadow-lg">
+                        Kumi – du grec « lève-toi » est la plateforme des soeurs de l'EJP Croissy
+                    </h2>
+                </div>
+
+                {/* Section EAGLES - Droite */}
+                <div className="relative min-h-[600px] lg:min-h-[700px] bg-gray-300 flex flex-col items-center justify-between py-12 px-8">
+                    {/* Titre */}
+                    <div className="text-center mb-8">
+                        <h2 className="text-4xl md:text-5xl lg:text-5xl font-bold text-white leading-tight drop-shadow-lg">
+                            TU ES UN FRÈRE ?<br />
+                            REJOINS LES EAGLES !
+                        </h2>
+                    </div>
+
+                    {/* Image */}
+                    <div className="flex-1 w-full max-w-2xl mb-8">
+                        <img
+                            src={unfrère}
+                            alt="Les Eagles"
+                            className="w-full h-full object-cover rounded-lg shadow-2xl"
+                        />
+                    </div>
+
+                    {/* Bouton */}
+                    <button className="px-8 py-4 border-2 border-white text-white rounded-md hover:bg-white hover:text-[#581c87] transition-all duration-300 font-medium text-lg flex items-center gap-2">
+                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
+                        </svg>
+                        Rejoindre ici
+                    </button><br />
+                    <h2 className="text-2xl md:text-2xl lg:text-2x1 font-bold text-gray-600 leading-tight drop-shadow-lg">
+                        Eagles – de l'anglais « aigle » est la plateforme des jeunes hommes de l'EJP Croissy
+                    </h2>
+                </div>
+        </section>
+    )
+}
+
+const Predictions= ()=>{
+    return(
+        <section
+            className="relative w-full h-screen flex flex-col items-center justify-center text-white"
+            style={{
+                backgroundImage:
+                "url('src/assets/predictions.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+            }}
+            >
+            {/* Overlay */}
+                <div className="absolute inset-0 bg-black/50"></div>
+
+                <div className="relative z-10 flex flex-col items-center px-4 text-center">
+                    <h2 className="text-3xl md:text-4xl font-bold tracking-wider">
+                        PRÉDICATIONS
+                    </h2>
+
+                    <p className="mt-2 text-sm md:text-base opacity-80">
+                        Retrouve toutes les prédications de notre berger sur Spotify
+                    </p>
+
+
+
+                    {/* Spotify button */}
+                    <a
+                        href="#"
+                        className="mt-8 inline-flex items-center gap-2 px-6 py-3 bg-white text-black font-semibold rounded-full shadow-lg hover:bg-gray-200 transition"
+                    >
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg"
+                            className="w-5 h-5"
+                            alt="spotify"
+                        />
+                        Spotify
+                    </a>
+                </div>
+        </section>
+    )
+}
+
 const Index = () => {
     return (
         <div className="font-['Inter','Segoe_UI',sans-serif]">
@@ -588,10 +698,9 @@ const Index = () => {
 
                     {/* Bible verse */}
                     <div className="text-base md:text-lg text-gray-300 leading-relaxed max-w-4xl mx-auto py-4">
-                        Lorsque tu donnes, fais-le en toute conscience et liberté,{' '}
-                        <span className="font-semibold text-white">avec joie</span>,{' '}
-                        <span className="font-semibold text-white">sans contrainte</span>, ni{' '}
-                        <span className="font-semibold text-white">regret</span>{' '}
+                        Que chacun donne comme il l'a résolu en son cœur, sans <span className="font-semibold text-white"> tristesse</span> ni{' '}  
+                        <span className="font-semibold text-white">contrainte</span> {' '}; car Dieu aime celui qui donne{' '}
+                        <span className="font-semibold text-white">avec joie</span>.{' '}<br />
                         <span className="text-gray-400 italic">(2 Corinthiens 9:7)</span>
                     </div>
 
@@ -652,7 +761,7 @@ const Index = () => {
                     </p>
 
                     <p className="text-base sm:text-lg md:text-xl text-white mb-16 max-w-5xl mx-auto leading-relaxed px-4">
-                        Les membres de l'église se réunissent <span className="font-bold">tous les mardis de 19h15 à 20h30</span> dans les maisons hôtes pour s'édifier et influencer leurs villes avec les valeurs de Christ. (Actes 5:42)
+                        Les Jeunes de l'église se réunissent <span className="font-bold">tous les mardis de 19h15 à 20h30</span> dans les maisons hôtes pour s'édifier et influencer leurs villes avec les valeurs de Christ. (Actes 5:42)
                     </p>
 
                     {/* Buttons */}
@@ -685,78 +794,6 @@ const Index = () => {
                     </div>
                 </div>
             </section>
-
-
-            <div className="w-full bg-white py-0">
-                <div className="container mx-auto px-4">
-                    <div className="w-full h-px bg-black"></div>
-                </div>
-            </div>
-
-            <section className="grid grid-cols-1 lg:grid-cols-2">
-                {/* Section KUMI - Gauche */}
-                <div className="relative min-h-[600px] lg:min-h-[700px] bg-gray-300 flex flex-col items-center justify-between py-12 px-8">
-                    {/* Titre */}
-                    <div className="text-center mb-8">
-                        <h2 className="text-4xl md:text-3xl lg:text-5xl font-bold text-white leading-tight drop-shadow-lg">
-                            TU ES UNE SŒUR ?<br />
-                            REJOINS LES KUMI !
-                        </h2>
-                    </div>
-
-                    {/* Image */}
-                    <div className="flex-1 w-full max-w-2xl mb-8">
-                        <img
-                            src={unesœur}
-                            alt="Les Kumi"
-                            className="w-full h-full object-cover rounded-lg shadow-2xl"
-                        />
-                    </div>
-
-                    {/* Bouton */}
-                    <button className="px-8 py-4 border-2 border-white text-white rounded-md hover:bg-white hover:text-[#1e3a8a] transition-all duration-300 font-medium text-lg flex items-center gap-2">
-                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
-                        </svg>
-                        Rejoindre ici
-                    </button><br />
-                    <h2 className="text-2xl md:text-2xl lg:text-2xl font-bold text-gray-600 leading-tight drop-shadow-lg">
-                        Kumi – du grec « lève-toi » est la plateforme des soeurs de l'EJP Croissy
-                    </h2>
-                </div>
-
-                {/* Section EAGLES - Droite */}
-                <div className="relative min-h-[600px] lg:min-h-[700px] bg-gray-300 flex flex-col items-center justify-between py-12 px-8">
-                    {/* Titre */}
-                    <div className="text-center mb-8">
-                        <h2 className="text-4xl md:text-5xl lg:text-5xl font-bold text-white leading-tight drop-shadow-lg">
-                            TU ES UN FRÈRE ?<br />
-                            REJOINS LES EAGLES !
-                        </h2>
-                    </div>
-
-                    {/* Image */}
-                    <div className="flex-1 w-full max-w-2xl mb-8">
-                        <img
-                            src={unfrère}
-                            alt="Les Eagles"
-                            className="w-full h-full object-cover rounded-lg shadow-2xl"
-                        />
-                    </div>
-
-                    {/* Bouton */}
-                    <button className="px-8 py-4 border-2 border-white text-white rounded-md hover:bg-white hover:text-[#581c87] transition-all duration-300 font-medium text-lg flex items-center gap-2">
-                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" />
-                        </svg>
-                        Rejoindre ici
-                    </button><br />
-                    <h2 className="text-2xl md:text-2xl lg:text-2x1 font-bold text-gray-600 leading-tight drop-shadow-lg">
-                        Eagles – de l'anglais « aigle » est la plateforme des jeunes hommes de l'EJP Croissy
-                    </h2>
-                </div>
-            </section>
-
             <br />
 
             {/* Trait de séparation */}
@@ -767,45 +804,6 @@ const Index = () => {
             </div>
 
             <br />
-
-
-            <section
-                className="relative w-full h-screen flex flex-col items-center justify-center text-white"
-                style={{
-                    backgroundImage:
-                        "url('src/assets/predictions.jpg')",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
-            >
-                {/* Overlay */}
-                <div className="absolute inset-0 bg-black/50"></div>
-
-                <div className="relative z-10 flex flex-col items-center px-4 text-center">
-                    <h2 className="text-3xl md:text-4xl font-bold tracking-wider">
-                        PRÉDICATIONS
-                    </h2>
-
-                    <p className="mt-2 text-sm md:text-base opacity-80">
-                        Retrouve toutes les prédications de notre berger sur Spotify
-                    </p>
-
-
-
-                    {/* Spotify button */}
-                    <a
-                        href="#"
-                        className="mt-8 inline-flex items-center gap-2 px-6 py-3 bg-white text-black font-semibold rounded-full shadow-lg hover:bg-gray-200 transition"
-                    >
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg"
-                            className="w-5 h-5"
-                            alt="spotify"
-                        />
-                        Spotify
-                    </a>
-                </div>
-            </section>
 
             <section id="contact" className="py-12 bg-white relative">
                 <Contact />
